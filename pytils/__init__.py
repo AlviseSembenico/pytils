1da6c1df3bfccb3c632b2eaea2e43126d3400308
Alvise <alvise@klue.com>
2021-11-12 21:18:13 +0100

    black format

--- pytils/__init__.py
+++ pytils/__init__.py
@@ -1,5 +1,5 @@
 """Top-level package for pytils."""
 
 __author__ = """Alvise Sembenico"""
-__email__ = 'alvise.sembenico@gmail.com'
-__version__ = '0.1.0'
+__email__ = "alvise.sembenico@gmail.com"
+__version__ = "0.1.0"
